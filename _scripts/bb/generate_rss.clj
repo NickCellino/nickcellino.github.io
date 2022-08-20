be16a71574e6a79c21b4a2b0863e83d34496b9d5
Nicholas Cellino <nacellin@buffalo.edu>
2022-08-20 11:08:38 -0400

    add an atom/rss feed

---
+++ _scripts/bb/generate_rss.clj
@@ -0,0 +1,22 @@
+(ns generate-rss
+  (:require [clj-rss.core :as rss]))
+
+(defn date
+  [year month day]
+  (.toInstant (java.util.Date. (- year 1900) (- month 1) day)))
+
+(defn generate []
+  (rss/channel-xml {:title "Nick Cellino's Website"
+                    :link "https://nickcellino.com"
+                    :description "Building simpler software."}
+                   {:title "Clojure Bandits"
+                    :guid "https://nickcellino.com/blog/2022-08-07-clojure-bandits.html"
+                    :link "https://nickcellino.com/blog/2022-08-07-clojure-bandits.html"
+                    :pubDate (date 2022 8 7)
+                    :description (str "An (interactive) exploration of a classic reinforcement learning problem"
+                                      " through the lens of Clojure.")}
+                   {:title "GraphQL, The N+1 Problem, and You"
+                    :guid "https://nickcellino.com/blog/2021-12-15-graphql-n-1.html"
+                    :link "https://nickcellino.com/blog/2021-12-15-graphql-n-1.html"
+                    :pubDate (date 2021 12 15)
+                    :description (str "Exploration of the N+1 problem in the context of GraphQL.")}))
